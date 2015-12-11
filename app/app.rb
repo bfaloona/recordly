6bf23c85a34cbd061f847f2ad890fa9686cb802a
Brandon Faloona <brandon@faloona.net>
2015-12-11 14:09:03 -0800

    Remove duplicate code

--- app/app.rb
+++ app/app.rb
@@ -5,25 +5,6 @@ module Recordly
     register Padrino::Helpers
     register Padrino::Warden
 
-    Warden::Strategies.add(:password) do
-      def valid?
-        params["email"] || params["password"]
-      end
-
-      def authenticate!
-        u = User.authenticate(params["email"], params["password"])
-        u.nil? ? fail!("Could not log in") : success!(u)
-      end
-    end
-
-    Warden::Manager.serialize_into_session do |user|
-      user.id
-    end
-
-    Warden::Manager.serialize_from_session do |id|
-      User.find(id)
-    end
-
     enable :sessions
 
   end
